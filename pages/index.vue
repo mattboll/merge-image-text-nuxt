0fe7ee464c139854db908a9c2d0e81403fd05d5f
Matthieu Bollot <m.bollot@code-troopers.com>
2024-06-14 09:03:22 +0200

    Pas le temps

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
         <input type="text" id="textInput" v-model="text" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" />
       </div>
       <div>
-        <label for="textInput">Couleur:</label>
+        <label for="textInput">Couleur du texte:</label>
         <input type="color" id="colorPicker" v-model="textColor" />
       </div>
       <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">Générer l'image</button>
